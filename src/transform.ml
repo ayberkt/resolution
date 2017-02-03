91087b8ae111f43a6c57ce0308c6df208b02ef6e
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-02-03 17:09:47 -0600

    Add some utilities

--- src/transform.ml
+++ src/transform.ml
@@ -8,5 +8,22 @@ module Transform = struct
         phi::(append (subforms alpha) (subforms beta))
     | Disj (alpha, beta) as phi ->
         phi::(append (subforms alpha) (subforms beta))
+    | Neg alpha as phi -> phi::subforms alpha
 
+  let rec max = function
+    | Prop i -> i
+    | Neg alpha -> max alpha
+    | Conj (alpha, beta) | Disj (alpha, beta) when max alpha > max beta ->
+        max alpha
+    | Conj (alpha, beta) | Disj (alpha, beta) -> max beta
+
+  let (=>) p q = Disj (Neg p, q)
+
+  let (<=>) p q = Conj (p => q, q => p)
+
+  (* let rec new_vars = function *)
+      (* | Prop i -> [] *)
+      (* | Conj (alpha, beta) -> *)
+          (* let nvars_alpha = new_vars alpha in *)
+        (* let nvars_beta  = new_vars beta in *)
 end
