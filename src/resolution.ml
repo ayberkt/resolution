0ba735512a8edfe8242464fafe391bde28e21160
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-02-25 14:06:52 -0600

    Implement resolve_all

--- src/resolution.ml
+++ src/resolution.ml
@@ -16,7 +16,9 @@ module Resolution = struct
   type clause = Clause.t
   type cnf_exp = clause array
 
-  type result = Refuted | Resolution of clause
+  type step = Box | Resolution of clause
+
+  type result = Refuted | Valid
 
   let rec get_lit_set : form -> clause = function
     | Prop p -> singleton (Positive p)
@@ -24,7 +26,7 @@ module Resolution = struct
     | Disj (p, q) -> union (get_lit_set p) (get_lit_set q)
     | _ -> failwith "this must not have happened"
 
-  let rec get_clauses : form -> cnf_exp = function
+  let rec get_clauses : form -> clause array = function
     | Conj (p, q) -> append (get_clauses p) (get_clauses q)
     | p -> Array.make 1 (get_lit_set p)
 
@@ -43,8 +45,41 @@ module Resolution = struct
       then empty
       else diff (union c1 c2) contradictions
 
-  let resolve_all cs =
-    let clauses : clause array = Array.of_list cs
-    in failwith "foo"
+  exception Found
 
+  let show_clause (c : clause) =
+    let proj = function
+      | Positive p -> p
+      | Negative p -> "~" ^ p
+    in
+    let ss = Clause.elements c in
+    "{" ^ (String.concat ", " (List.map proj ss)) ^ "}"
+
+  let resolve_all (cs : clause array) =
+    let _ =
+      Printf.printf "Clauses: ";
+      Array.iter (fun x -> Printf.printf "%s\n" (show_clause x)) cs in
+    let cs' = Array.map (fun x -> Some x) cs in
+    let clauses : (clause option) array = cs' in
+    let find_reso_bin i c1' j (c2' : clause option) =
+      match (c1', c2') with
+      | (Some c1, Some c2) ->
+          let _ =
+            Printf.printf "Comparing %s with %s.\n"
+            (show_clause c1) (show_clause c2) in
+          let r = resolve c1 c2 in
+          if is_empty r
+          then true
+          else
+            let _ = Array.set clauses i (Some r); Array.set clauses j None
+            in false
+      | _ -> false in
+  let rec existsi p xs =
+    let f x i = if p x i then raise Found else () in
+    try
+      Array.iteri f xs; false
+    with
+    | Found -> true in
+  let rec find_reso_bin_all i c = existsi (find_reso_bin i c) clauses in
+  if existsi find_reso_bin_all clauses then Refuted else Valid
 end
