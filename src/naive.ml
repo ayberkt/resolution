c4b71dddacd416a38c083c9968e8a5111c02532b
Ayberk Tosun <ayberk.tosun@gmail.com>
2017-02-25 00:43:32 -0600

    Fix CNF transformation

--- src/naive.ml
+++ src/naive.ml
@@ -4,35 +4,26 @@ module Naive : TRANSFORM = struct
   open List
   open AbsResolution
 
-  let rec distribute = function
+  let rec transform = function
+    | Neg Neg p -> transform p
+    | Neg Conj (p, q) -> Disj (transform (Neg p), transform (Neg q))
+    | Neg Disj (p, q) -> Conj (transform (Neg p), transform (Neg q))
     | Disj (p, Conj (q, r)) ->
-        let phi   = distribute (Disj (p, q)) in
-        let theta = distribute (Disj (p, r)) in
-        Conj (phi, theta)
+        Conj (transform (Disj (p, q)), transform (Disj (p, r)))
     | Disj (Conj (p, q), r) ->
-        let phi   = distribute (Disj (p, r)) in
-        let theta = distribute (Disj (q, r)) in
-        Conj (phi, theta)
-    | Disj (p, q) -> Disj (distribute p, distribute q)
-    | Conj (p, q) -> Conj (distribute p, distribute q)
-    | Neg p -> Neg (distribute p)
+        Conj (transform (Disj (p, r)), transform (Disj (q, r)))
+    | Neg p -> Neg (transform p)
+    | Disj (p, q) -> Disj (transform p, transform q)
+    | Conj (p, q) -> Conj (transform p, transform q)
     | Prop _ as p -> p
 
-  let rec push_neg = function
-    | Neg Conj (p, q) -> Disj (push_neg (Neg p), push_neg (Neg q))
-    | Neg Disj (p, q) -> Conj (push_neg (Neg p), push_neg (Neg q))
-    | Conj (p, q) -> Conj (push_neg p, push_neg q)
-    | Disj (p, q) -> Disj (push_neg p, push_neg q)
-    | Neg _ as p -> p
-    | Prop _ as p -> p
-
-  let rec omit_double_neg = function
+  (* let rec omit_double_neg = function
     | Neg Neg p -> omit_double_neg p
     | Conj (p, q) -> Conj (omit_double_neg p, omit_double_neg q)
     | Disj (p, q) -> Disj (omit_double_neg p, omit_double_neg q)
     | Neg p -> Neg (omit_double_neg p)
     | Prop _ as p -> p
-
-  let rec transform p = omit_double_neg p |> push_neg |> distribute
+ *)
+  (* let rec transform p = transform (transform p) *)
 
 end
